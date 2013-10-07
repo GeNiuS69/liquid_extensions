5ee218d8a20b83069515191ed646c0a9bd7e93ab
Alexandr Nigomatulin <nigomatulin@gmail.com>
2013-10-07 15:57:20 +0400

    page info

--- lib/locomotive/liquid_extensions/tags/info.rb
+++ lib/locomotive/liquid_extensions/tags/info.rb
@@ -10,10 +10,10 @@ module Locomotive
 
         def display(name)
             page = Page.all.where(title: name).first
-            if page.title = 'Главная'
-            "#{page.title} #{ page.created_at.in_time_zone('Moscow').strftime('Создано: %R %d.%m.%Y') }" + "<br> #{ DateTime.parse(page.updated_at).in_time_zone('Moscow').strftime('Обновлено: %R %d.%m.%Y') }"
+            if page.title == 'Главная'
+            "#{ page.created_at.in_time_zone('Moscow').strftime('Создано: %R %d.%m.%Y') }" + "<br> #{ DateTime.parse(page.updated_at).in_time_zone('Moscow').strftime('Обновлено: %R %d.%m.%Y') }"
             else
-            "#{page.title} #{ page.created_at.in_time_zone('Moscow').strftime('Создано: %R %d.%m.%Y') }" + "<br> #{ page.updated_at.in_time_zone('Moscow').strftime('Обновлено: %R %d.%m.%Y') }"
+            "#{ page.created_at.in_time_zone('Moscow').strftime('Создано: %R %d.%m.%Y') }" + "<br> #{ page.updated_at.in_time_zone('Moscow').strftime('Обновлено: %R %d.%m.%Y') }"
 
             end
 
