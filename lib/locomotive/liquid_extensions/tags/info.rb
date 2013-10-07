417409429521469557a3c4a65f3307c5127fc625
Alexandr Nigomatulin <nigomatulin@gmail.com>
2013-10-07 15:44:32 +0400

    page info

--- lib/locomotive/liquid_extensions/tags/info.rb
+++ lib/locomotive/liquid_extensions/tags/info.rb
@@ -10,7 +10,7 @@ module Locomotive
 
         def display(name)
             page = Page.all.where(title: name).first
-            "<p>#{ page.created_at.in_time_zone('Moscow').strftime('Создано: %R %d.%m.%Y') }</p><p> #{ page.updated_at.in_time_zone('Moscow').strftime('Обновлено: %R %d.%m.%Y') }</p>"
+            "<p>#{ page.created_at.in_time_zone('Moscow').strftime('Создано: %R %d.%m.%Y') }</p>" + "<p> #{ page.updated_at.in_time_zone('Moscow').strftime('Обновлено: %R %d.%m.%Y') }</p>"
 
         end
 
